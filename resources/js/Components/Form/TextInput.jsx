f58a2a6c813549a32b5ab76427bc8c07bfa0ab83
Abd. Asis <id.abdasis@gmail.com>
2024-01-10 12:01:11 +0700

    Updated: Form Text Input

--- resources/js/Components/Form/TextInput.jsx
+++ resources/js/Components/Form/TextInput.jsx
@@ -5,41 +5,23 @@ import {Button} from "@/Components/ui/button.jsx";
 import {Label} from "@/Components/ui/label.jsx";
 import {Input} from "@/Components/ui/input.jsx";
 import {cn} from "@/Lib/Utils.jsx.js";
-import {Alert, AlertDescription, AlertTitle} from "@/Components/ui/alert.jsx";
-import {RocketIcon} from "@radix-ui/react-icons";
+import LabelHelp from "@/Components/Form/LabelHelp.jsx";
 
 const TextInput = forwardRef(function TextInput(props, ref) {
     const {label, name, errors, placeholder, help, icon, className, iconPosition = 'start', ...otherProps} = props;
-    const FormHelp = (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button variant="ghost" className={'p-0 h-0'}>
-                    <IconHelp size={14} stroke={1}/>
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 p-0 border-slate-100 shadow-sm">
-                <div className="pop-header text-xs font-semibold p-2 px-3 flex gap-1 border-b border-slate-100 items-center">
-                    <IconHelp size={16}/>
-                    <span>Bantuan</span>
-                </div>
-                <div className="pop-body p-2 px-3">
-                    <small>{help}</small>
-                </div>
-            </PopoverContent>
-        </Popover>
-    );
+
     return (
-        <Fragment>
+        <>
             <Label htmlFor={name} className="flex items-center gap-1 mb-1">
                 <span>{label}</span>
                 {help && (
-                    FormHelp
+                    <LabelHelp text={help}/>
                 )}
             </Label>
             {icon != null ? (
-                <Fragment>
+                <>
                     {iconPosition == 'end' ? (
-                        <div className={cn('flex focus-within:border-1 focus-within:border-rose-300 shadow-sm focus-within:shadow-none border rounded-md px-3 items-center gap-1' + className)}>
+                        <div className={cn('flex h-9 focus-within:border-1 focus-within:border-primary shadow-sm focus-within:shadow-none border rounded-md pe-3 items-center gap-1' + className)}>
                             <Input
                                 placeholder={placeholder}
                                 className={'border-0 focus-visible:ring-0 shadow-none'}
@@ -50,7 +32,7 @@ const TextInput = forwardRef(function TextInput(props, ref) {
                             <span className="input-icon-addon">{icon}</span>
                         </div>
                     ) : (
-                        <div className={cn('flex focus-within:border-1 focus-within:border-rose-300 shadow-sm focus-within:shadow-none border rounded-md px-3 items-center gap-1' + className)}>
+                        <div className={cn('flex h-9  focus-within:border-1 focus-within:border-primary shadow-sm focus-within:shadow-none border rounded-md ps-3 items-center gap-1' + className)}>
                             <span className="input-icon-addon">{icon}</span>
                             <Input
                                 placeholder={placeholder}
@@ -61,10 +43,11 @@ const TextInput = forwardRef(function TextInput(props, ref) {
                             />
                         </div>
                     )}
-                </Fragment>
+                </>
             ) : (
                 <div>
                     <Input
+                        className={'h-9'}
                         placeholder={placeholder}
                         id={name}
                         name={name}
@@ -73,7 +56,7 @@ const TextInput = forwardRef(function TextInput(props, ref) {
                 </div>
             )}
             {errors && <small className="text-danger">{errors}</small>}
-        </Fragment>
+        </>
     );
 });
 
