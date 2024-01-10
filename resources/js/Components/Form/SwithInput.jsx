0c3d59c7829c4d1524f4ca60ce08d3933f6a64aa
Abd. Asis <id.abdasis@gmail.com>
2024-01-10 14:57:07 +0700

    Added: Swith Input

--- resources/js/Components/Form/SwithInput.jsx
+++ resources/js/Components/Form/SwithInput.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {Switch} from "@/Components/ui/switch.jsx";
 import {Label} from "@/Components/ui/label.jsx";
 
-const SwitchInput = ({label, name, errors, placeholder, help, ...props}) => {
+const SwitchInput = ({label, value, name, errors, placeholder, onCheck, help, ...props}) => {
     return (
-        <div className="flex items-center space-x-2">
-            <Switch id={name}/>
-            <Label htmlFor={name}>{label}</Label>
-        </div>
+        <>
+            <Label className={'mb-10'} htmlFor={name}>{label}</Label>
+            <div className="flex items-center space-x-2">
+                <Switch onCheckedChange={(e) => !onCheck ? null : onCheck(e)} id={name}/>
+                <Label htmlFor={name}>{value}</Label>
+            </div>
+        </>
     );
 };
 
